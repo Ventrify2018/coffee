cd79ba2cfba4524ade0ad6518d9d428f8800aca1
xwafflecakex <(nyevtukh@gmail.com)>
2019-10-26 19:29:15 -0600

    Added register button linked to register page

--- CoffeeWebapp/Coffee_Webapp/views.py
+++ CoffeeWebapp/Coffee_Webapp/views.py
@@ -9,3 +9,7 @@ def index(requests):
 
 def login(requests):
     return render(requests, 'Coffee_Webapp/login/login.html')
+
+
+def register(requests):
+    return render(requests, 'Coffee_Webapp/Register/register.html')
